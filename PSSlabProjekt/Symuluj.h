445a39f46ee090106dded6a5043eeaaeb9e97081
dani <danital>
2023-05-29 12:35:08 +0200

    Klasa symuluj

--- PSSlabProjekt/Symuluj.h
+++ PSSlabProjekt/Symuluj.h
@@ -3,12 +3,19 @@
 #include "Plik_JSON.h"
 #include "Regulator.h"
 #include <iostream>
+#include "P.h"
+#include "PI.h"
+#include "PD.h"
+#include "PID.h"
 
+enum class typRegulatora:char{
+	P,PI,PD,PID
+};
 
 class Symuluj
 {
 public:
-	Symuluj();
+	Symuluj(typRegulatora typ);
 	~Symuluj();
 
 private:
